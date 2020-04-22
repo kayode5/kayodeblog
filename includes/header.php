f56610243778aa8127ded0c0fd96286a6608776d
kayode5 <42555955+kayode5@users.noreply.github.com>
2020-04-22 16:46:20 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -15,9 +15,11 @@
                             });
                         });
                         </script>
+    <?php
     header("X-Frame-Options: sameorigin");
     header("X-XSS-Protection: 1; mode=block");
     header("X-Content-Type-Options: nosniff");
+    ?>
 
     <!-- Title -->
     <title>Kayode's Blog</title>
